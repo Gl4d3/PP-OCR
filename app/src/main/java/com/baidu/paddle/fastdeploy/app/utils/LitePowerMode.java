fb01582b9fa61242b2fec572a4ad449a58ba5c2c
Victor Mutuku <victin250@gmail.com>
2024-09-02 19:11:35 +0300

    Added necessary missing files

---
+++ app/src/main/java/com/baidu/paddle/fastdeploy/app/utils/LitePowerMode.java
@@ -0,0 +1,11 @@
+package com.baidu.paddle.fastdeploy.app.utils;
+
+public enum LitePowerMode {
+    LITE_POWER_HIGH,
+    LITE_POWER_LOW,
+    LITE_POWER_FULL,
+    LITE_POWER_NO_BIND,
+    LITE_POWER_RAND_HIGH,
+    LITE_POWER_RAND_LOW
+}
+
---
+++ app/src/main/java/com/baidu/paddle/fastdeploy/app/utils/PPOCRVersion.java
@@ -0,0 +1,7 @@
+package com.baidu.paddle.fastdeploy.app.utils;
+
+public enum PPOCRVersion {
+    OCR_V1,
+    OCR_V2,
+    OCR_V3
+}
